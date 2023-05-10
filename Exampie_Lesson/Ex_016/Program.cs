9da2957eca5d981cd45b1f961462c755725192db
Yvagaemiy <krylatiy2@mail.ru>
2023-05-10 14:10:38 +0300

    Example/исправил

--- Exampie_Lesson/Ex_016/Program.cs
+++ Exampie_Lesson/Ex_016/Program.cs
@@ -34,20 +34,28 @@ int screenHeightPosition = Console.WindowHeigh  / 2;*/
 
 using System.Linq;
 
-
 string text = "(1,2) (3,4) (5,6) (7,8)"
-        .RepLace("(", "")
-        .RepLace(")", "")
+        .Replace ("(", "")
+        .Replace (")", "")
         ;
 
 Console.WriteLine(text);
 var data = text.Split(" ")
-
+            .Select (item=> item.Split(','))
+            .Select (e=>(x: int.Parse(e[0]), y: int.Parse(e[1]) ))
             .ToArray();
 
 for (int i = 0;i < data.Length; i++ ) 
 {
-Console.WriteLine(data[i]);
+   // for ( int k = 0; k < data[i].Length ; k++)
+      //  {
+       //         Console.WriteLine(data[i][k]);
+      //  }
+    //Console.WriteLine(); 
 
+    Console.WriteLine(data[i]. x * 10);
+    
+     Console.WriteLine(data[i]. y + 2);
+     Console.WriteLine();
 }           
-   
+
