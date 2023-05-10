6f872e36e7e9caba0bcd3924085675562c89e8d8
Yvagaemiy <krylatiy2@mail.ru>
2023-05-10 17:53:11 +0300

    ex_001 собрать строку из чисел

---
+++ Example_Recursia/Ex_001 recursia/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//Собрать строку с числами от a до b, а Б=b
+string NumbersFor (int a , int b)
+{
+    string result = String.Empty;
+
+    for (int i = a ;a < b;i++)
+    {
+        //Console.WriteLine(Numbers);
+        result += $"{i}";
+    }
+      return result;
+}
+string NumbersRec (int a , int b)
+{
+     if ( a<=b ) return $"{a}" + NumbersRec(a+1 , b);//надо доделать
+     else return String.Empty;
+}
+Console.WriteLine(NumbersFor(1 , 10));
+Console.WriteLine(NumbersRec(1 , 10));
+
+
+
+
+
