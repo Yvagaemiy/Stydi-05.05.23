83c0bb417e4b5a051b5694d5f7e58e7fa4c0126e
Yvagaemiy <krylatiy2@mail.ru>
2023-05-11 13:29:59 +0300

    ex_005 перебор слов

---
+++ Example_Recursia/Ex_005_перебор слов/Program.cs
@@ -0,0 +1,23 @@
+﻿//Перебор слов
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        char[] MassivSimvol = { 'a', 'и', 'в', 'с' };
+
+        int count = MassivSimvol.Length;
+
+        int n = 1;
+
+        for (int i = 0; i < count; i++)
+        {
+
+
+            for(int j=0;j < count;j++)
+            {
+                Console.WriteLine($"{n++,-5}{MassivSimvol[i]}{MassivSimvol[j]}");
+            }
+        }
+    }
+}
