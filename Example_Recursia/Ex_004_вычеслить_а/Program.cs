bcd67e19d77da8f0adcc89fc9744d22da299e72c
Yvagaemiy <krylatiy2@mail.ru>
2023-05-11 12:14:21 +0300

    ex_004 вычисли а

---
+++ Example_Recursia/Ex_004_вычеслить_а/Program.cs
@@ -0,0 +1,17 @@
+﻿// ВЫчислить а
+
+
+int PowerFor(int a ,int n)
+{
+   int resulte = 1;
+    for (int i = 1;i<= n;i++) resulte *= a;
+    return resulte;
+}
+int PowerRec(int a, int n)
+{
+   if (n==0) return 1;
+   else return PowerRec(a, n-1)* a;
+}
+Console.WriteLine(PowerFor(2, 10));
+
+Console.WriteLine(PowerRec(2, 10));
