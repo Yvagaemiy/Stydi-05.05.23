879fece59d01630ce500d38fd054c7381a32b059
Yvagaemiy <krylatiy2@mail.ru>
2023-05-11 14:23:35 +0300

    ex_006 разбиение на пары

---
+++ Example_Recursia/Ex_006_разбиение_на_пары/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = 1;
+void FindWords(string aлфовит, char[] слова,int length = 0)
+{
+    if(length == слова.Length)
+    {
+            Console.WriteLine($"{n++}{new String(слова)}");
+            return;
+    }
+    for(int i =0; i < aлфовит.Length;i++)
+    {
+        слова[length] = aлфовит[i];
+        FindWords( aлфовит,  слова, length + 1);
+    }
+}
+
+FindWords("алиса" , new char[2]);
