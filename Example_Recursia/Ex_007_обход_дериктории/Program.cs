46b613fae6305817b2c4ed7c888598aa3394b673
Yvagaemiy <krylatiy2@mail.ru>
2023-05-11 15:42:44 +0300

    ex_007 обхад дериктории 2 методая

--- Example_Recursia/Ex_007_обход_дериктории/Program.cs
+++ Example_Recursia/Ex_007_обход_дериктории/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Обход дериктории
 /*
-string path = "C:/Users/Уважаемый/Desktop/програмирование/5A знакомство с базами данных/сименар3";
+string path = "C:/Users/Уважаемый/Desktop/програмирование/4A язык программирование/1 лекция/7 рекурсия продолжение";
 DirectoryInfo di = new DirectoryInfo(path);
 System.Console.WriteLine(di.CreationTime);
 FileInfo[] fi = di.GetFiles();
@@ -10,3 +10,21 @@ for (int i = 0; i < fi.Length; i++)
     System.Console.WriteLine(fi[i].Name);
 }
 */
+//рекурсия которая  смотрит все папки
+
+
+void CatalogInfo(string path, string indent = "")
+{
+ DirectoryInfo catalogs = new DirectoryInfo(path);
+ foreach (var currentCatalog in catalogs.GetDirectories())
+ {
+ Console.WriteLine($"{indent}{currentCatalog.Name}");
+ CatalogInfo(currentCatalog.FullName, indent + " ");
+ }
+ foreach (var item in catalogs.GetFiles())
+ {
+ Console.WriteLine($"{indent}{item.Name}");
+ }
+}
+string path = "C:/Users/Уважаемый/Desktop/програмирование/4A язык программирование/3 практика и ДЗ/Stydi-05.05.23";
+CatalogInfo(path);
