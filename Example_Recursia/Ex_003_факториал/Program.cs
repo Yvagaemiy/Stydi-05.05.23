c501c964b2fa25419afb2ad1a02adc435277e2ce
Yvagaemiy <krylatiy2@mail.ru>
2023-05-11 11:56:06 +0300

    ex_003 факториал

---
+++ Example_Recursia/Ex_003_факториал/Program.cs
@@ -0,0 +1,16 @@
+﻿//Факториал
+
+int FactorialFor(int n)
+{
+   int resulte = 1;
+   for( int i =1;i <= n ;i++) resulte *=i;
+   return resulte;
+}
+int FactorialRec(int n)
+{
+    if(n==1) return 1;
+    else return n* FactorialRec(n-1);
+}
+
+Console.WriteLine(FactorialFor(10));
+Console.WriteLine(FactorialRec(10));
