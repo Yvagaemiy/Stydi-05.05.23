445ac6905a87bf7c07e37953c51bb204f6d3bcba
Yvagaemiy <krylatiy2@mail.ru>
2023-05-11 11:42:28 +0300

    ex_002 cумма чисел от 1 до n

---
+++ Example_Recursia/Ex_002/Program.cs
@@ -0,0 +1,17 @@
+﻿//Сумма чисел от 1 до n
+
+
+int SumFor(int n)
+{
+    int resulte = 0;
+    for( int i = 1;i <= n;i ++) resulte +=i;
+    return resulte;
+}
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n-1);
+}
+Console.WriteLine(SumFor(10));
+
+Console.WriteLine(SumRec(10));
