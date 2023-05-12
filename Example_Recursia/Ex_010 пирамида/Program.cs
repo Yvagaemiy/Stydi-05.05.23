99798cab9b8dc0f2270b80da1a082c730e83983b
Yvagaemiy <krylatiy2@mail.ru>
2023-05-12 10:25:23 +0300

    ex_010 пирамидка/дороботанная

--- Example_Recursia/Ex_010 пирамида/Program.cs
+++ Example_Recursia/Ex_010 пирамида/Program.cs
@@ -1,12 +1,13 @@
 ﻿//Пирамида
 
 
-void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+void Towers(string with = "1", string on = "4", string some = "2",string some1 = "3", int count = 5)
 {
- if (count > 1) Towers(with, some, on, count - 1);
+ if (count > 1) Towers(with, some,some1, on, count - 1);
  
  Console.WriteLine($"{with} >> {on}");
  
- if (count > 1) Towers(some, on, with, count - 1);
+ if (count > 1) Towers(some,some1, on, with, count - 1);
 }
 
+Towers();
